f553f881583d5428a6a575a1455bc4e3d3c275af
ammaribrahim-rpl <ammar01ibrahim@gmail.com>
2025-08-11 09:41:13 +0700

    background_added

--- lib/pages/name_card.dart
+++ lib/pages/name_card.dart
@@ -6,37 +6,43 @@ class NameCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Siswa',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/img/background.png', fit: BoxFit.cover),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Siswa',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+                SizedBox(height: 20),
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/img/profile_img.jpeg'),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  'Ammar Ibrahim',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'Website & Flutter Developer',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    background: Paint()
+                      ..color = Colors.red
+                      ..style = PaintingStyle.fill,
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('assets/img/profile_img.jpeg'),
-            ),
-            SizedBox(height: 15),
-            Text(
-              'Ammar Ibrahim',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 5),
-            Text(
-              'Website & Flutter Developer',
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-                background: Paint()
-                  ..color = Colors.blue!
-                  ..style = PaintingStyle.fill,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
